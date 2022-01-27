0fd65be2c203aa7841c8571739583f03230eeafd
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-01-27 13:55:45 -0500

    feat: erc1155 facade and test

--- py_order_utils/facades/erc20_facade.py
+++ py_order_utils/facades/erc20_facade.py
@@ -1,26 +1,21 @@
-from enum import Enum
-import json
-import os
-from web3 import Web3
-
 from py_order_utils.utils import normalize_address
+from py_order_utils.facades.base_facade import BaseFacade
 
-transferFrom = 'transferFrom'
-balanceOf = 'balanceOf'
-erc20Abi = "abi/ERC20ABI.json"
 
-class Erc20Facade:
+class Erc20Facade(BaseFacade):
+    
+    ABIS = {"erc20": "abi/ERC20ABI.json"}
+    transferFrom = "transferFrom"
+    balanceOf = "balanceOf"
 
-    def __init__(self, abi_file_path=erc20Abi):
-        self.abi_file_path = abi_file_path
-        self.web3 = Web3()
-        self.contract = None
+    def __init__(self):
+        super().__init__(self.ABIS)
 
     def transfer_from(self, from_address, to_address, value : str):
         """
         Creates transaction data for an ERC20 transferFrom
         """
-        return self._get_contract().encodeABI(fn_name=transferFrom, 
+        return self._get_contract("erc20").encodeABI(fn_name=self.transferFrom, 
         args=[
             normalize_address(from_address), 
             normalize_address(to_address), 
@@ -32,20 +27,4 @@ class Erc20Facade:
         """
         Creates transaction data for an ERC20 balanceOf
         """
-        return self._get_contract().encodeABI(fn_name=balanceOf, args=[normalize_address(address)])
-
-    def _get_contract(self):
-        if not self.contract:
-            self.contract = self.web3.eth.contract(None, abi=self._get_abi())
-        return self.contract
-    
-    def _get_abi(self):
-        file_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            '..',
-            self.abi_file_path,
-        )
-
-        with open(file_path, "r") as fh:
-            abi = json.load(fh)
-        return abi
+        return self._get_contract("erc20").encodeABI(fn_name=self.balanceOf, args=[normalize_address(address)])
