ff47eb52a4f90617c3d6bb1c8d2b49ab47b97915
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-02-03 14:44:36 -0500

    chore: dependency fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_order_utils",
-    version="0.0.3",
+    version="0.0.4",
     author="Jonathan Amenechi",
     author_email="jonathanamenechi@gmail.com",
     description="Python utilities used to generate and sign limit and market orders on Polymarket's CLOB",
@@ -14,8 +14,13 @@ setuptools.setup(
     url="https://github.com/polymarket/python-order-utils",
     install_requires=[
         'web3>=5.0.0,<6.0.0',
-        'eip712-structs>=1.0.0',
+        'eth-account>=0.4.0,<0.6.0',
+        'eip712-structs',
         'pytest'
+        'eth-abi',
+        'eth_typing',
+        'eth_utils',
+        'eth_utils',
     ],
     project_urls={
         "Bug Tracker": "https://github.com/polymarket/python-order-utils",
