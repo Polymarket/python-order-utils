0a916c3a92e9895ca9853eaeabc0e27a96ad870a
mike <mshrieve@gmail.com>
2023-10-24 11:34:04 -0400

    add ContractConfig class

--- py_order_utils/config.py
+++ py_order_utils/config.py
@@ -1,23 +1,11 @@
-class ContractConfig:
-    def __init__(self, exchange, collateral, conditional):
-        self.exchange = exchange
-        self.collateral = collateral
-        self.conditional = conditional
-
-    def get_exchange(self):
-        return self.exchange
-
-    def get_collateral(self):
-        return self.collateral
-
-    def get_conditional(self):
-        return self.conditional
+from .model import ContractConfig
 
 
-def get_contract_config(chainID: int) -> ContractConfig:
+def get_contract_config(chainID: int, neg_risk: bool=False) -> ContractConfig:
     """
     Get the contract configuration for the chain
     """
+
     CONFIG = {
         137: ContractConfig(
             exchange="0x4bFb41d5B3570DeFd03C39a9A4D8dE6Bd8B8982E",
@@ -31,7 +19,23 @@ def get_contract_config(chainID: int) -> ContractConfig:
         ),
     }
 
-    config = CONFIG.get(chainID)
+    NEG_RISK_CONFIG = {
+        137: ContractConfig(
+            exchange="",  # TODO
+            collateral="",  # TODO
+            conditional="0x4D97DCd97eC945f40cF65F87097ACe5EA0476045",
+        ),
+        80001: ContractConfig(
+            exchange="",  # TODO
+            collateral="",  # TODO
+            conditional="0x7D8610E9567d2a6C9FBf66a5A13E9Ba8bb120d43",
+        ),
+    }
+
+    if neg_risk:
+        config = NEG_RISK_CONFIG.get(chainID)
+    else:
+        config = CONFIG.get(chainID)
     if config is None:
         raise Exception("Invalid chainID: ${}".format(chainID))
 
--- py_order_utils/model/__init__.py
+++ py_order_utils/model/__init__.py
@@ -1,3 +1,4 @@
 from py_order_utils.model.order import OrderData, Order, SignedOrder
 from py_order_utils.model.signatures import EOA, POLY_PROXY, POLY_GNOSIS_SAFE
 from py_order_utils.model.sides import BUY, SELL
+from py_order_utils.model.contract_config import ContractConfig
