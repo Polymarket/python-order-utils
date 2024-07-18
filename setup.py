92df98c2d1f456cfabb010089176cca5dd1e3192
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2024-07-18 11:52:12 -0400

    fix: version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_order_utils",
-    version="0.3.0",
+    version="0.3.1",
     author="Polymarket Engineering",
     author_email="engineering@polymarket.com",
     maintainer="Polymarket Engineering",
